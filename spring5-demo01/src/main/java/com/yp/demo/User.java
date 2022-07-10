ef2d4bd398efd716af8ee0a302d4c882eb4998b0
Catcheryp <2281718195@qq.com>
2022-07-10 19:24:15 +0800

    spring5入门示例01

---
+++ spring5-demo01/src/main/java/com/yp/demo/User.java
@@ -0,0 +1,15 @@
+package com.yp.demo;
+
+public class User {
+
+//    private String userName;
+//
+//    public User(String userName) {
+//        this.userName = userName;
+//    }
+
+    public void add() {
+        System.out.println("add......");
+    }
+}
+
