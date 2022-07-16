862549cbe70aa8191e13311e77aa9965e9f45f67
Catcheryp <2281718195@qq.com>
2022-07-16 23:18:14 +0800

    IOC-完全注解开发

--- spring5-annotation01/src/main/java/com/yp/demo/testdemo/TestSpring5Annotation.java
+++ spring5-annotation01/src/main/java/com/yp/demo/testdemo/TestSpring5Annotation.java
@@ -1,7 +1,10 @@
 package com.yp.demo.testdemo;
 
+import com.yp.demo.config.SpringConfig;
+import com.yp.demo.dao.UserDao;
 import com.yp.demo.service.UserService;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring5Annotation {
@@ -10,6 +13,15 @@ public class TestSpring5Annotation {
     public void testAnnotation1(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         UserService userService = context.getBean("userService", UserService.class);
+        System.out.println(userService);
+        userService.add();
+    }
+
+    @Test
+    public void testAnnotation2(){
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        UserService userService = context.getBean("userService", UserService.class);
+
         System.out.println(userService);
         userService.add();
     }
