44edc66cb68aacccd5b245b14df75ba24ca169ad
Catcheryp <2281718195@qq.com>
2022-07-24 08:44:03 +0800

    springsecurity-简单示例，比较完整的例子，基本涵盖了SpringSecurity核心的所有部分

---
+++ springsecurity-demo01/src/main/java/com/yp/springsecuritydemo01/SpringsecurityDemo01Application.java
@@ -0,0 +1,12 @@
+package com.yp.springsecuritydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityDemo01Application.class, args);
+	}
+}
